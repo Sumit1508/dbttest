68a90cff45eeb1bc4c40ef4e28a2bf9094a08ea6
Sumit Manral <smanral@accurate.com>
2025-04-16 10:34:49 +0000

    bu

--- models/warehouse/DWH_Package_dbtpoc.sql
+++ models/warehouse/DWH_Package_dbtpoc.sql
@@ -2,9 +2,10 @@
     materialized='incremental',
     alias='DWH_Package_DBTPOC',
     unique_key='package_code',
-    on_schema_change='sync_all_columns',
-    post_hook=["{{ ref('DWH_Package_DBTPOC_Merge') }}"]  -- Reference the separate MERGE model here
-)}}
+    on_schema_change='sync_all_columns'
+    
+) }}
+
 
 SELECT * 
 FROM {{ ref('Stg_Package_dbtpoc') }}
