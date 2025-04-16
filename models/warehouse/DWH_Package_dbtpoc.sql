3a94d07d07c8e943070b25f364db65c1e28f59fe
Sumit Manral <smanral@accurate.com>
2025-04-16 05:42:00 +0000

    v

--- models/warehouse/DWH_Package_dbtpoc.sql
+++ models/warehouse/DWH_Package_dbtpoc.sql
@@ -5,10 +5,67 @@
     on_schema_change='sync_all_columns'
 ) }}
 
-SELECT *
+WITH staged AS (
+
+    SELECT
+        *,
+        MD5(TO_JSON(OBJECT_CONSTRUCT(
+            'package_code', package_code,
+            'pkg_name', pkg_name,
+            'pkg_name_short', pkg_name_short,
+            'pkg_desc', pkg_desc,
+            'pkg_price', pkg_price,
+            'pkg_active', pkg_active,
+            'allow_ala_carte', allow_ala_carte,
+            'init_comp', init_comp,
+            'search_type_code', search_type_code,
+            'yr_searched', yr_searched,
+            'pkg_plus', pkg_plus,
+            'no_addl_inof_needed', no_addl_inof_needed,
+            'no_fm', no_fm,
+            'name_count', name_count,
+            'county_count', county_count,
+            'ask_conv', ask_conv,
+            'MMN_SHOW', MMN_SHOW,
+            'ADD_POSITION_LOCATION_SEARCHES', ADD_POSITION_LOCATION_SEARCHES,
+            'auto_W2_opt', auto_W2_opt,
+            'auto_W2_utv', auto_W2_utv,
+            'intl_address_question', intl_address_question,
+            'sin_required', sin_required
+        ))) AS row_hash
     FROM ACCEL_BI_STG.stg_package_dbtpoc
 
+)
+
+SELECT *
+FROM staged
 
 {% if is_incremental() %}
-  WHERE updated_at >= (SELECT MAX(updated_at) FROM {{ this }})
+WHERE row_hash NOT IN (
+    SELECT MD5(TO_JSON(OBJECT_CONSTRUCT(
+        'package_code', package_code,
+        'pkg_name', pkg_name,
+        'pkg_name_short', pkg_name_short,
+        'pkg_desc', pkg_desc,
+        'pkg_price', pkg_price,
+        'pkg_active', pkg_active,
+        'allow_ala_carte', allow_ala_carte,
+        'init_comp', init_comp,
+        'search_type_code', search_type_code,
+        'yr_searched', yr_searched,
+        'pkg_plus', pkg_plus,
+        'no_addl_inof_needed', no_addl_inof_needed,
+        'no_fm', no_fm,
+        'name_count', name_count,
+        'county_count', county_count,
+        'ask_conv', ask_conv,
+        'MMN_SHOW', MMN_SHOW,
+        'ADD_POSITION_LOCATION_SEARCHES', ADD_POSITION_LOCATION_SEARCHES,
+        'auto_W2_opt', auto_W2_opt,
+        'auto_W2_utv', auto_W2_utv,
+        'intl_address_question', intl_address_question,
+        'sin_required', sin_required
+    ))) 
+    FROM {{ this }}
+)
 {% endif %}
