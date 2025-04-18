13e56d10598a6495a3945d4151d76c00558cd6d8
Sumit Manral <smanral@accurate.com>
2025-04-18 06:17:44 +0000

    c

--- macros/merge.sql
+++ macros/merge.sql
@@ -1,14 +1,13 @@
 {% macro merge_into(target_table, source_table, unique_key, update_columns, insert_columns) %}
-
     MERGE INTO {{ target_table }} AS target
     USING {{ source_table }} AS source
     ON target.{{ unique_key }} = source.{{ unique_key }}
-
+    
     WHEN MATCHED THEN UPDATE SET
         {% for col in update_columns %}
             target.{{ col }} = source.{{ col }}{% if not loop.last %}, {% endif %}
         {% endfor %}
-
+    
     WHEN NOT MATCHED THEN INSERT (
         {% for col in insert_columns %}
             {{ col }}{% if not loop.last %}, {% endif %}
@@ -19,5 +18,4 @@
             source.{{ col }}{% if not loop.last %}, {% endif %}
         {% endfor %}
     );
-
 {% endmacro %}
