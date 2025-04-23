58fb685717452286f81dcf6cfaab5fef532e7d2d
Sumit Manral <smanral@accurate.com>
2025-04-23 10:17:03 +0000

    bh

--- models/warehouse/DWH_DimOrder_dbtpoc.sql
+++ models/warehouse/DWH_DimOrder_dbtpoc.sql
@@ -5,26 +5,26 @@
     on_schema_change='sync_all_columns',
     post_hook=[
         "{{ merge_with_logging(
-            'DEV.ACCEL_BI_BR.DWH_DimOrder_dbtpoc',
+            'DEV.ACCEL_BI_BR.DWH_DimOrder',
             'DEV.ACCEL_BI_STG.Stg_DimOrder',
             'PackageID',
             [
                 'OrderModeType', 'OrderModeSubType', 'OrderOriginCode', 'OrderOriginSubCode',
-                'OrderType', 'OrdeSubType', 'PackageCode', 'OrderDate', 'ClientRefNum',
-                'CompletionDate', 'Status', 'SubStatus', 'ResultSentDate', 'LocaleCode',
+                'OrderType', 'OrderSubType', 'OrderDate', 'ClientRefNum',
+                'CompletionDate', 'Status', 'SubStatus', 'ResultSentDate', 
                 'InvoiceDatetime', 'NotMonthlyInvoiceDatetime', 'ResearcherSummaryDate',
                 'ResultNote', 'StatusNote', 'EmailSent', 'AdjudicationDescription',
                 'NeedReviewEmailSent', 'OINEmailSent', 'PkgProcessed', 'InsertAuditKey',
                 'UpdateAuditKey', 'DisputedStatus', 'DisputedDate', 'DOC_FEE', 'STATUTORY_FEE',
-                'addl_year_fee', 'COPIES_FEE', 'RefNumber2', 'RefNumber3', 'RefNumber4',
+                'ADDITIONAL_YEAR_FEE', 'COPIES', 'RefNumber2', 'RefNumber3', 'RefNumber4',
                 'RefNumber5', 'SEARCHID', 'ABStartDate', 'ABEndDate', 'CompleteDate',
                 'CannedNote', 'AdjTriggerReason', 'PackageCompleted10Days', 'CandidateDisputedName',
                 'PackageReopenInd', 'PackageReopenTime', 'PastDueSearchCount', 'ClientRefNum6',
                 'ClientRefNum7', 'ClientRefNum8', 'ClientRefNum9', 'SearchOrderCount',
                 'OINSearchCount', 'IsOIN', 'IsRN', 'AdjudicationNote', 'FinalAdjudicatorName',
                 'NeedsReviewAdjudicatorName', 'ABReviewerName', 'InvitationEmailSent',
-                'PackageProcessed', 'DatePlacedIntoNeedReview', 'OrderInitiationDate',
-                'adj_grid_id', 'adj_grid_name','PackageKey'
+                'PackageProcessed', 'DatePlacedIntoNeedReview', 'OR_OrderInitDate',
+                'AdjGridId', 'AdjGridName'
             ]
         ) }}"
     ]
