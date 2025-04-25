ba548c0b615c48e18df632ded6d146b4b4a8b21a
Sumit Manral <smanral@accurate.com>
2025-04-25 17:48:58 +0000

    nj

---
+++ macros/log_run_results.sql
@@ -0,0 +1,22 @@
+{% macro log_run_results(results) %}
+  {% for r in results %}
+    {% if r.status != 'success' %}
+      {% set log_sql %}
+        INSERT INTO DEV.ACCEL_LOGGINGDB.DBT_MODEL_ERRORS (
+          model_name,
+          status,
+          message,
+          run_by,
+          run_time
+        ) VALUES (
+          '{{ r.node.name }}',
+          '{{ r.status }}',
+          '{{ r.message | replace("'", "") }}',
+          '{{ target.user }}',
+          CURRENT_TIMESTAMP()
+        )
+      {% endset %}
+      {% do run_query(log_sql) %}
+    {% endif %}
+  {% endfor %}
+{% endmacro %}
